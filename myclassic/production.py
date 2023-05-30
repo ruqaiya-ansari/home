55c9e3b2bec6b0badb6d92b96311f2454240432c
ruqaiya-ansari <133018079+ruqaiya-ansari@users.noreply.github.com>
2023-05-30 23:54:31 +0530

    Update production.py
    ckeditor path issue

--- myclassic/production.py
+++ myclassic/production.py
@@ -29,6 +29,9 @@ STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
 AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_IMAGE_BACKEND = 'pillow'
+
 
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
